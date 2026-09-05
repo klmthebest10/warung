8ea4bc94382ff966b7bf46de11c32f8bc3d5dda6
klmthebest10 <kholillini45@gmail.com>
2026-09-05 12:38:32 +0900

    sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'warung-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install Service Worker dan simpan cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Gunakan cache jika ada, jika tidak fetch dari internet
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
